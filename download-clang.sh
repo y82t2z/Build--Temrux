3635a079f93a8b703f43283e987ac4d5137f1fb8
NyaruToru <nyakoriandnyarutoru@gmail.com>
2024-11-24 04:51:53 +0700

    Update

--- download-clang.sh
+++ download-clang.sh
@@ -1,6 +1,5 @@
 mkdir -p "neutron-clang"
 cd "neutron-clang"
 bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
-
 echo "Downloaded Neutron-clang Successful"
 cd ../
