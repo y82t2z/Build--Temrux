6cd5cc2663b52a100349e1514991db49c14a7d2c
NyaruToru <nyakoriandnyarutoru@gmail.com>
2024-11-24 07:03:33 +0700

    Update Runner & Clang

--- download-clang.sh
+++ download-clang.sh
@@ -1,5 +1,6 @@
 mkdir -p "neutron-clang"
 cd "neutron-clang"
 bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
+bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
 echo "Downloaded Neutron-clang Successful"
 cd ../
