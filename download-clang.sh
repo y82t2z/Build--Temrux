e77c7be55ef6ecdafed69bde7d39912e3a65ffce
NyaruToru <nyakoriandnyarutoru@gmail.com>
2024-11-25 07:52:23 +0700

    Init Eva GCC

--- download-clang.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p "neutron-clang"
-cd "neutron-clang"
-bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
-bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
-echo "Downloaded Neutron-clang Successful"
-cd ../
