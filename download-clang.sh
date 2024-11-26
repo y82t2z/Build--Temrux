238b7d0359715d9c0e22dfc1a5f49addc19dc041
y82t2z <nyakoriandnyarutoru@gmail.com>
2024-11-26 14:56:18 +0700

    update nv

--- download-clang.sh
+++ download-clang.sh
@@ -1,6 +1,6 @@
 mkdir clang
 cd clang
-wget -i <(wget -qO- https://raw.githubusercontent.com/ZyCromerZ/Clang/refs/heads/main/Clang-main-link.txt) -O clang.tar.gz
+wget -nv -i <(wget -qO- https://raw.githubusercontent.com/ZyCromerZ/Clang/refs/heads/main/Clang-main-link.txt) -O clang.tar.gz
 tar -xvf clang.tar.gz
 echo "Downloaded Clang Successful"
 cd ../
