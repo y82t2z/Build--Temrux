8e974850f0e4752568759bd0505e1cb9bb4106e2
y82t2z <nyakoriandnyarutoru@gmail.com>
2024-12-10 13:14:17 +0700

    update

--- download-clang.sh
+++
@@ -1,6 +0,0 @@
-mkdir clang
-cd clang
-bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
-bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
-echo "Downloaded Clang Successful"
-cd ../
